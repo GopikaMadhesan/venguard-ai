4aea6b988d6321fae018c687dbe3f3d3de133311
Gopika Madhesan <gopikagops05@gmail.com>
2026-05-14 11:37:57 +0530

    Final VenGuard AI deployment

--- app.py
+++ app.py
@@ -396,9 +396,6 @@ def logout():
 # RUN APPLICATION
 # ==========================================
 
+app.run(debug=True)
 if __name__ == '__main__':
-    app.run(
-        debug=True,
-        host="0.0.0.0",
-        port=10000
-    )
+    app.run(host='0.0.0.0', port=10000)
